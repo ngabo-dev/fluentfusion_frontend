a537794604b14ca58119a89a01891ab751e4bfa2
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-10-13 12:46:45 +0200

    Working with text

--- src/components/ContactDiv.jsx
+++ src/components/ContactDiv.jsx
@@ -4,7 +4,7 @@ function ContactDiv() {
   return (
     <div style={{width:'96%',height:'100%', paddingLeft:'18px', paddingRight:'18px', borderLeft:'black 2px solid'}}>
     <h3 style={{ textDecoration:'underline'}}>
-        TWIGE URURIME RW'AMARENGA
+        TWIGE URURIMI RW'AMARENGA
       </h3>
       <p style={{textAlign:'justify'}}>
       Mu rwego rwo guteza imbere ururimi rw’amarenga ndetse no gutezimbere ababana n'ubumuga bwo kutumva, IITECH RWANDA yiyemeje kubagezaho urubuga rwigisha imyandikire y’ururimi rw'amarenga kubuntu.
@@ -21,7 +21,7 @@ function ContactDiv() {
     +(250) 788 737 639 <br /><br />
 
     <b> Website:</b> <br/>
-    www.iitech.rw
+    <a href='https://www.iitech.rw/' style={{color:'black', textDecoration:'none'}} target='_blank'>www.iitech.rw </a>
     </div>
   )
 }
