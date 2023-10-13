364c6357f0fce5eb66137fefa79dd75925bdaa9e
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-10-13 14:14:50 +0200

    Current year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <div style={{backgroundColor:'#4890fc', width:'100%', minHeight:"40px", color:'white', textAlign:'center', bottom:'0', left: '0', position:'fixed'}}>
         <div style={{marginTop:'12px'}}>
           Copyright &copy; {currentYear} all rights reserved. Made by
-         <a href='https://iitech.rw/'  target='_blank' style={{ color: 'white', textDecoration:'none', marginLeft:'2px', fontWeight:'bold' }}> IITECH RWANDA </a>
+         <a href='https://iitech.rw/'  target='_blank' style={{ color: 'white', textDecoration:'underline', marginLeft:'4px', fontWeight:'bold', textUnderlineOffset:'5px' }}>IITECH RWANDA</a>
 
 
         </div>
