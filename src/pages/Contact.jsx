03aa6de75d03aecbda568ada031594afd4e2c4d3
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-10-14 19:01:11 +0200

    Contact us

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,19 @@ function Contact() {
     <Container>
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
         <div style={{ flex: '1 0 70%' }}>
-          Contact us now
+          <div style={{width:'92%', paddingLeft:'4%'}}>
+            <h3 style={{textDecoration:'underline', width:'100%', textAlign:'center'}} > TWOHEREZE IGITEKEREZO </h3>
+            <p style={{fontSize:'18px'}}> Amazina yombi:</p>
+            <input type='text' style={{width:'90%', backgroundColor:'#EEEEEE', borderRadius:'4px', height:'32px'}} />
+            <p style={{fontSize:'18px'}}> Email:</p>
+            <input type='email' style={{width:'90%', backgroundColor:'#EEEEEE', borderRadius:'4px', height:'32px'}} />
+            <p style={{fontSize:'18px'}}> Ubutumwa:</p>
+            <textarea type='email' style={{width:'90%', backgroundColor:'#EEEEEE', borderRadius:'4px'}} rows={4}></textarea> <br />
+
+            <input type='submit' style={{backgroundColor:'#4890fc', height:'36px', color:'white', borderRadius:'4px', fontWeight:'bold', marginTop:'18px'}} value={'Ohereza'} />
+
+
+          </div>
         </div>
         <div style={{ flex: '1 0 26%', marginLeft: '12px' }}>
           <ContactDiv />
