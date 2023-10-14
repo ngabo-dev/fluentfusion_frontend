2d0b636fffcc501d3bdd01c14fdb4905672dc9f9
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-10-14 19:19:26 +0200

    Amasomo

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -7,7 +7,13 @@ function Lessons() {
     <div>
 
     <Container>
-        Lessons feature is under construction!
+      <div style={{alignItems:'flex'}}>
+
+        <div style={{width:'20%', borderRight:'gray 2px solid'}} >
+          <h3 style={{textAlign:'center'}}> IBYICIRO </h3>
+
+        </div>
+      </div>
     </Container>
     </div>
   )
