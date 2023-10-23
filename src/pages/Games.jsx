8e962b5a1f16ca2636f9e928ce4f7587ffe6631e
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-10-23 09:59:25 +0200

    Debug numbering

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -6,7 +6,24 @@ function Games() {
     <div>
 
     <Container>
-        Games feature is under construction
+    <div style={{display:'flex'}}>
+
+    <div style={{width:'20%', borderRight:'gray 2px solid', minHeight:'420px'}} >
+      <h3 style={{textAlign:'center', }}> IMYITOZO </h3>
+      <ul>
+        <li style={{textDecoration:'none', marginTop:'26px'}}> Icyiciro cya 1 (A - D)</li>
+        <li style={{textDecoration:'none', marginTop:'18px'}}> Icyiciro cya 2 (E - G)</li>
+        <li style={{textDecoration:'none', marginTop:'18px'}}> Icyiciro cya 3 (H - K)</li>
+        <li style={{textDecoration:'none', marginTop:'18px'}}> Icyiciro cya 4 (L - O)</li>
+        <li style={{textDecoration:'none', marginTop:'18px'}}> Icyiciro cya 5 (P - T)</li>
+        <li style={{textDecoration:'none', marginTop:'18px'}}> Icyiciro cya 6 (U - Z)</li>
+      </ul>
+
+    </div>
+    <div>
+      akajkja
+    </div>
+    </div>
     </Container>
     </div>
   )
