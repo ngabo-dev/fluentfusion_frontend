34c52f795f893466ae4f4f65a81209a5c52b8696
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-11-02 00:42:08 +0200

    Simple explanation text

--- src/components/ContactDiv.jsx
+++ src/components/ContactDiv.jsx
@@ -7,19 +7,20 @@ function ContactDiv() {
         TWIGE URURIMI RW'AMARENGA
       </h3>
       <p style={{textAlign:'justify'}}>
-      Mu rwego rwo guteza imbere ururimi rw’amarenga ndetse no gutezimbere ababana n'ubumuga bwo kutumva, IITECH RWANDA yiyemeje kubagezaho urubuga rwigisha imyandikire y’ururimi rw'amarenga kubuntu.
+       TWIGE AMARENGA n'urubuga mugezwaho na IITECH RWANDA rwigisha ururimi rw'amarenga kubuntu, muguteza imbere ababana n'ubumuga bwo kutumva.
       </p>
 
     <h4 style={{ textDecoration:'underline'}}>
-      CONTACTS
+      DUHAMAGARE
     </h4>
+
+    <b> Telephone:</b> <br/>
+    <a href='https://wa.me/250788737639' style={{color:'black', textDecoration:'none'}} target='_blank'> +(250) 788 737 639 </a> <br /><br />
+
     <b> Email:</b> <br/>
     <a href='mailto:info@iitech.rw' style={{color:'black', textDecoration:'none'}} target='_blank'>
       info@iitech.rw  </a> <br /><br />
 
-
-    <b> Telephone:</b> <br/>
-    <a href='https://wa.me/250788737639' style={{color:'black', textDecoration:'none'}} target='_blank'> +(250) 788 737 639 </a> <br /><br />
 
     <b> Website:</b> <br/>
     <a href='https://www.iitech.rw/' style={{color:'black', textDecoration:'none'}} target='_blank'>www.iitech.rw </a>
