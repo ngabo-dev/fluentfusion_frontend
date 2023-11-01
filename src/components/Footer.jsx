af65f988fef8984f18e5e90b95e87c45fb3e5300
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-11-02 00:21:43 +0200

    Correct responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <div className='the-footer'>
-        <div style={{marginTop:'12px'}}>
+        <div style={{marginTop:'14px', marginBottom:'12px'}}>
           Copyright &copy; {currentYear}, all rights reserved. Made by
          <a href='https://iitech.rw/'  target='_blank' style={{ color: 'white', textDecoration:'underline', marginLeft:'4px', fontWeight:'bold', textUnderlineOffset:'5px' }}>IITECH RWANDA</a>
 
