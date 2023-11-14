a1478836adb98145aa361d42ba6f4122c6632487
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-11-14 18:00:23 +0200

    make better images

---
+++ src/constants/pngnumbers.js
@@ -0,0 +1,13 @@
+import one from "../assets/pngnumbers/1.jpeg"
+import two from "../assets/pngnumbers/2.jpeg"
+import three from "../assets/pngnumbers/3.jpeg"
+import four from "../assets/pngnumbers/4.jpeg"
+import five from "../assets/pngnumbers/5.jpeg"
+import six from "../assets/pngnumbers/6.jpeg"
+import seven from "../assets/pngnumbers/7.jpeg"
+import eight from "../assets/pngnumbers/8.jpeg"
+import nine from "../assets/pngnumbers/9.jpeg"
+import zero from "../assets/pngnumbers/zero.jpeg"
+
+
+export { one, two, three, four, five, six, seven, eight, nine, zero }
